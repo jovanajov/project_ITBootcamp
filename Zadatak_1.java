2ad32c961fe4a7beb63e90423c86b566e2d37008
Jovana Jovanovic <jovjovana998@gmail.com>
2021-10-07 20:21:52 +0200

    Prvi commit

---
+++ Zadatak_1.java
@@ -0,0 +1,24 @@
+package p16_08_2021;
+
+import java.util.Scanner;
+
+public class Zadatak_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner(System.in);
+		System.out.println("Upisi prvi broj:  ");
+		int a = s.nextInt();
+		System.out.println("Upisi drugi broj: ");
+		
+		int b = s.nextInt();
+
+    
+		if (a <= b) {
+			System.out.println("broj je manji");
+		}
+
+	}
+
+}
